1036f9237ef6d19f8f153534529c5a1210f008e4
DeathCharger <75020525+DeathCharger@users.noreply.github.com>
2021-11-12 21:55:32 +0530

    Update alive.js

--- alive.js
+++ alive.js
@@ -1,6 +1,7 @@
 var http = require('http');  
+
 http.createServer(function (req, res) {   
-    res.write("DeathCharger pro");  
+    res.write("BOT STARTED!!");  
     res.end(); 
   }).listen(8080);
 
