1c7572ba76ae32a71ae26b63f717dfc4bf3e85a6
DeathCharger <75020525+DeathCharger@users.noreply.github.com>
2021-11-12 21:53:03 +0530

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const { prefix } = require(`../../config.json`)
+
+
+module.exports = {
+	name: 'help',
+	description: 'Help command',
+	guildOnly: true,
+	execute(message, args) { 
+        const helpmenu = new Discord.MessageEmbed()
+        .setColor(`BLUE`)
+        .setTitle(`Help Menu`)
+        .addField(`${prefix}ping`, `Latency of Bot`)
+        .addField(`${prefix}ticket setup`, `Creates an embed with a "Create Ticket" button. Users can click on this button to open tickets`)
+        .addField(`${prefix}ticket close`, `Sends an embed with "Close Ticket" button. Deletes the ticket`)
+
+
+        message.channel.send(helpmenu)
+    }}
